705f039b2e8cd0342fe1e5325a303ce1492e1f73
andy04177 <anandkumar04177@gmail.com>
2023-09-28 14:45:09 +0530

    my first commit

---
+++ sum.py
@@ -0,0 +1,12 @@
+# Function to add two numbers
+def add_numbers(num1, num2):
+    result = num1 + num2
+    return result
+
+# Input from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Call the add_numbers function and display the result
+sum_result = add_numbers(num1, num2)
+print(f"The sum of {num1} and {num2} is {sum_result}")
